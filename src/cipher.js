726abe4e04fe595f799526184a7938f73bc5ec50
Jess Inga Mayhua <jessinma40@gmail.com>
2022-10-28 23:19:43 -0500

    Implementada la funcionalidad de cifrado (captura clave, captura texto, boton Cifrar, muestra texto cifrado)

--- src/cipher.js
+++ src/cipher.js
@@ -1,18 +1,23 @@
 const cipher = {
+  encode: function(offset, string) {
+    let textEncode = "";
+    for (let index = 0; index < string.length; index++) {
+
+      // Si es un espacio, imprimir un espacio
+
+      
+      let codeChar = string.charCodeAt(index);
+      let codeCharEncode = (codeChar - 65 + offset) % 26 + 65;
+      let charCipher = String.fromCharCode(codeCharEncode);
+      textEncode = textEncode + charCipher;
+    }
+    return textEncode;
+  }
+
 // cipher.encode(offset, string)
 // cipher.decode(offset, string)
 
-  // funcion para encriptar
-
-    // Ciclo para recorrer por cada caracter
-      // Reemplazar letra por letraCifrada
-        // Obtener codigo ASCII
-        // Obtener desplazamiento elegido por usuario
-
-
-  // Mostrar resultado
-  // Limpiar texto de textarea
-
+ 
 
 };
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import cipher from './cipher.js';
 // Obtener el elemento button desde html para trabajarlo en js
 let btnEncrypt = document.getElementById("btnEncrypt");
-// Agregar un escuchador de eventos al boton para que ejecute una accion (mediante la funcion )
+// Agregar un escuchador de eventos al boton para que ejecute una accion (mediante una funcion )
 btnEncrypt.addEventListener("click", encryptText);
 
 function encryptText() {
+    // Obtener el VALOR del input (clave)
+    let keyEncrypt = document.getElementById("key").value;
+    
     // Obtener el VALOR del elemento textarea desde html para trabajarlo en js
     let txtToCipher = document.getElementById("textToEncrypt").value;
+
+    // Llama al metodo del objeto cipher (para encriptar)
+    let textEncode = cipher.encode(keyEncrypt, txtToCipher);
+
     // Muestra el valor del textarea
-    document.getElementById("result").innerHTML = txtToCipher;
+    document.getElementById("result").innerHTML = textEncode;
+    
 }
+
+
 
 //console.log(cipher);
