b96bad6e655d9eb391c86dcc9a3d2553d7edca82
Jess Inga Mayhua <jessinma40@gmail.com>
2022-10-31 12:04:34 -0500

    Implementada función que permite la entrada (via textarea) solo de LETRAS MAYUSCULAS sin Ñ

--- src/cipher.js
+++ src/cipher.js
@@ -2,26 +2,13 @@ const cipher = {
   encode: function(offset, string) {
     let textEncode = "";
     for (let index = 0; index < string.length; index++) {
-
-      // Si es un espacio, imprimir un espacio
-
-      
       let codeChar = string.charCodeAt(index);
-      console.log(codeChar);
-      let codeCharEncode = ((codeChar - 65 + offset) % 26) + 65;
-      console.log(codeChar - 65 + offset);
+      let codeCharEncode = (codeChar - 65 + offset) % 26 + 65;
       let charCipher = String.fromCharCode(codeCharEncode);
-      console.log(charCipher);
       textEncode = textEncode + charCipher;
     }
     return textEncode;
   }
-
-// cipher.encode(offset, string)
-// cipher.decode(offset, string)
-
- 
-
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,54 @@
 import cipher from './cipher.js';
-// Obtener el elemento button desde html para trabajarlo en js
+
+// Obtener el elemento input (clave)
+let keyEncrypt = document.getElementById("key");
+// Obtener el elemento textarea
+let textToCipher = document.getElementById("textToEncrypt");
+// Agregar escuchador al textarea
+textToCipher.addEventListener("keydown", checkChar);
+// Agregar escuchador al textarea
+// textToCipher.addEventListener("input", detectChanges)
+// Obtener el elemento p (resultado)
+let containerTxtEncode = document.getElementById("result");
+
+
+
+// Obtener el elemento button
 let btnEncrypt = document.getElementById("btnEncrypt");
 // Agregar un escuchador de eventos al boton para que ejecute una accion (mediante una funcion )
 btnEncrypt.addEventListener("click", encryptText);
+// btnEncrypt.addEventListener("click", allowCharacter);
+
+
+// String con los caracteres permitidos, incluye espacio
+let charAllowed = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+
+function checkChar(e) {
+
+    if(charAllowed.includes(e.key)){
+    } else {
+        e.preventDefault()
+    }
+}
 
 function encryptText() {
-    // Obtener el VALOR del input (clave)
-    let keyEncrypt = parseInt(document.getElementById("key").value);
+    // Obtener el VALOR de la clave, y usar parseInt para cambiar de string a number
+    keyEncrypt = parseInt(keyEncrypt.value);
+    // Obtener el VALOR del elemento textarea 
+    textToCipher = textToCipher.value;
     
-    // Obtener el VALOR del elemento textarea desde html para trabajarlo en js
-    let txtToCipher = document.getElementById("textToEncrypt").value;
-
     // Llama al metodo del objeto cipher (para encriptar)
-    let textEncode = cipher.encode(keyEncrypt, txtToCipher);
-
-    // Muestra el valor del textarea
-    document.getElementById("result").innerHTML = textEncode;
-    
+    let textEncode = cipher.encode(keyEncrypt, textToCipher);
+    // Muestra el valor del texto encriptado/desencriptado
+    containerTxtEncode.innerHTML = textEncode;
 }
 
 
 
-//console.log(cipher);
+// function detectChanges(event) {
+//     // Node.textContent cambia el contenido del nodo (elemento p)
+//     // Con event obtenemos las propiedades del evento. La propiedad target (objetivo) hace referencia al input y value al contenido el input
+//     containerTxtEncode.textContent = event.target.value;
+// }
+
+
