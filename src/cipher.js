52567640150a77cd06a446c44b55ad6dc5c169fa
Jess Inga Mayhua <jessinma40@gmail.com>
2022-11-07 10:50:36 -0500

    Avance sin lograr pasar test

--- src/cipher.js
+++ src/cipher.js
@@ -1,13 +1,16 @@
 const cipher = {
   encode: function(offset, string) {
-    let textEncode = "";
-    for (let index = 0; index < string.length; index++) {
-      let codeChar = string.charCodeAt(index);
-      let codeCharEncode = (codeChar - 65 + offset) % 26 + 65;
-      let charCipher = String.fromCharCode(codeCharEncode);
-      textEncode = textEncode + charCipher;
+    if(typeof offset === "number" && typeof string === "string"){
+      let textEncode = "";
+      for (let index = 0; index < string.length; index++) {
+        let codeChar = string.charCodeAt(index);
+        let codeCharEncode = (codeChar - 65 + offset) % 26 + 65;
+        let charCipher = String.fromCharCode(codeCharEncode);
+        textEncode = textEncode + charCipher;
+      }
+      return textEncode;
     }
-    return textEncode;
+    
   },
 
   decode: function(offset, string) {
